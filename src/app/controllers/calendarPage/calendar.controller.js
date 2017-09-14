445ef718a425fd4e9bb54d97a66d999d4322ff8e
Danilo Silva <danil@NBQSP-FC174.fcamara.com.br>
2017-09-14 19:51:06 -0300

    calendar page

--- src/app/controllers/calendarPage/calendar.controller.js
+++ src/app/controllers/calendarPage/calendar.controller.js
@@ -1,19 +1,12 @@
 import config from 'config/config';
 
-// import heroPicture from 'img/main_photo.jpg';
-// import feature1Picture from 'img/manSide.jpg';
-// import feature2Picture from 'img/boxTwo.jpg';
-// import feature3Picture from 'img/boxOne.jpg';
-// import sentencePicture from 'img/stylist.jpg';
-
 const CONTROLLER_NAME = 'CalendarController';
 
 function calendarController($timeout, $http, $state) {
     const vm = this;
 
-    vm.countryCodes = ['Germany (+49)', 'Brazil (+55)'];
-    vm.callFromStylist = true;
-    vm.obs = "";
+    vm.countryCodes = [{ code: '+49', label: 'Germany (+49)'}, { code:'+55', label:  'Brazil (+55)'}];
+    vm.countryCode = { code: '+49', label: 'Germany (+49)'};
 
     $http.get('https://nrg-frontend-task-api.herokuapp.com/timeslots').then(
         (response) => {
@@ -28,7 +21,7 @@ function calendarController($timeout, $http, $state) {
             date: vm.currentDate,//'yyyy-MM-dd', 
             slot: vm.currentTime,//'HH:mm', 
             orderConfirmationComment: vm.obs,//'TEXT', 
-            phone: vm.phoneNumber//'+4901234567890'
+            phone: vm.countryCode.code + vm.phoneNumber//'+4901234567890'
         };
         if (vm.callFromStylist){
             $http.post('https://nrg-frontend-task-api.herokuapp.com/appointments', appointment).then(
@@ -46,11 +39,6 @@ function calendarController($timeout, $http, $state) {
     function submitAction(appointment){        
         $state.go('successPage', {obj: appointment});
     }
-//   vm.heroPicture = heroPicture;
-//   vm.feature1Picture = feature1Picture;
-//   vm.feature2Picture = feature2Picture;
-//   vm.feature3Picture = feature3Picture;
-//   vm.sentencePicture = sentencePicture;
 }
 
 calendarController.$inject = ['$timeout', '$http', '$state'];
--- src/app/controllers/calendarPage/index.js
+++ src/app/controllers/calendarPage/index.js
@@ -1,29 +1,30 @@
 import angular from 'angular';
-import psLogger from 'shared/services/util/ps-logger';
-import psAbLogger from 'shared/services/util/ps-abLogger';
+// import psLogger from 'shared/services/util/ps-logger';
+// import psAbLogger from 'shared/services/util/ps-abLogger';
 import psNavHeader from 'shared/components/navHeader';
 import psFooter from 'components/footer';
-import psSectionHero from 'components/sectionHero';
-import psSectionBulletPoints from 'components/sectionBulletPoints';
-import psSectionFeature from 'components/sectionFeature';
-import psSectionSentence from 'components/sectionSentence';
+// import psSectionHero from 'components/sectionHero';
+// import psSectionBulletPoints from 'components/sectionBulletPoints';
+// import psSectionFeature from 'components/sectionFeature';
+// import psSectionSentence from 'components/sectionSentence';
 import { calendarControllerName, calendarController } from './calendar.controller';
 import dropdown from 'shared/components/dropdown'
 import orderBtn from 'shared/components/orderBtn'
+import './calendar.style.sass';
 //import phoneNumberInput from 'shared/directives/phoneNumberInput/phoneNumberInput.js'
 
 const MODULE_NAME = 'CalendarController';
 
 angular.module(MODULE_NAME, [
   // Internal dependencies
-  psLogger,
-  psAbLogger,
+  // psLogger,
+  // psAbLogger,
   psNavHeader,
   psFooter,
-  psSectionHero,
-  psSectionBulletPoints,
-  psSectionFeature,
-  psSectionSentence,
+  // psSectionHero,
+  // psSectionBulletPoints,
+  // psSectionFeature,
+  // psSectionSentence,
   dropdown,
   orderBtn,
   //phoneNumberInput,
